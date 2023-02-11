ea539a18deac6941adf26ca2f9a8a3591e16c721
AriaWorkshop <ariaworkshopnft@gmail.com>
2023-02-11 16:10:48 +0200

    more shipping logic

--- blockchain/contracts/MarketPlace.sol
+++ blockchain/contracts/MarketPlace.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
@@ -16,38 +15,33 @@ contract Marketplace is ERC721, ERC721URIStorage, Ownable {
     event ItemCreated(uint _id, address seller);
     event ItemSold(uint _id, address buyer);
     event SellerWithdraw(uint amount, address withdrawer);
+    event OrderStatusUpdated(ShippingStatus ShippingStatus, uint id, address seller, address buyer);
 
 
-    //When seller creates item, mint that nft and let him set a price for the item 
-    //When buyer buys item, mint nft and add to the balance of the seller the amount set
-    //let seller of items withdraw the balance
-    //Let owner of the marketplace withdraw the balance
-    //keep track of ids, every seller that creates an item will get a new id(incrementally)
 
-
-    //todo 
-    //1. Image upload logic , with ipfs ( when mint upload image , or second step ? I think second step)
-    //2. Shipment status returns string
-    //3. think about events
-    //4. read more state to string to read in the front end
-    //5. bought item state for buyer, he will be able to view the items that he bought and shippment status in his page , custom route with address
-    //6. shipment address can be viewed only by seller that sold item to the buyer
     uint public currentId;
 
 
     // address of user pointing to ID of item pointing to the sale price of ID of item
     mapping(address => mapping(uint => uint)) userInventory;
+    //uint (ID) points to shipping status
+    mapping(uint => ShippingStatus) orderStatus;
     // balance of user, address points to value
     mapping(address => uint) balanceUser;
+    // id points to buyer;
+    mapping(uint => address) buyerOfId;
+
     address[] public sellers;
 
    enum ShippingStatus {
+       OrderNotActive,
        OrderReceived,
        ShippingInProgress,
        Shipped
    }
 
     function CreateItemToSell(uint sellPrice) public payable {
+        require(sellPrice != 0, "Price can't be 0");
         setApprovalForAll(address(this), true);
         currentId++;
         userInventory[msg.sender][currentId] = sellPrice;
@@ -56,10 +50,12 @@ contract Marketplace is ERC721, ERC721URIStorage, Ownable {
         emit ItemCreated(currentId, msg.sender);
     }
 
-    function BuyItem(uint _id, address seller, uint _amount) public payable {
-       require(msg.value * _amount == userInventory[seller][_id] * _amount, "inssuficient ammount");
+    function BuyItem(uint _id, address seller) public payable {
+       require(msg.value  == userInventory[seller][_id], "inssuficient ammount");
        ERC721(address(this)).safeTransferFrom(seller, msg.sender, _id);
+       orderStatus[_id] = ShippingStatus.OrderReceived;
        balanceUser[seller] += msg.value;
+       buyerOfId[_id] = msg.sender;
        emit ItemSold(_id, msg.sender);
     }
 
@@ -99,6 +95,23 @@ contract Marketplace is ERC721, ERC721URIStorage, Ownable {
           }
     }
 
+    function getShipmentStatusForId(uint _id) external view returns(ShippingStatus shippingStatus) {
+       shippingStatus = orderStatus[_id];
+       return shippingStatus;
+    }
+
+    function updateShipmentStatus(uint _id) external {
+        require(userInventory[msg.sender][_id] > 0, "user doesn't own the item");
+        orderStatus[_id] = ShippingStatus.ShippingInProgress;
+        emit OrderStatusUpdated(ShippingStatus.ShippingInProgress, _id, msg.sender, buyerOfId[_id]);
+    }
+
+      function finalizeOrder(uint _id) external {
+        require(userInventory[msg.sender][_id] > 0, "user doesn't own the item");
+        orderStatus[_id] = ShippingStatus.Shipped;
+        emit OrderStatusUpdated(ShippingStatus.Shipped, _id, msg.sender, buyerOfId[_id]);
+    }
+ 
         function tokenURI(uint256 tokenId)
         public
         view
@@ -113,3 +126,19 @@ contract Marketplace is ERC721, ERC721URIStorage, Ownable {
     }
 
 }
+
+
+    //When seller creates item, mint that nft and let him set a price for the item 
+    //When buyer buys item, mint nft and add to the balance of the seller the amount set
+    //let seller of items withdraw the balance
+    //Let owner of the marketplace withdraw the balance
+    //keep track of ids, every seller that creates an item will get a new id(incrementally)
+
+
+    //todo 
+    //1. Image upload logic , with ipfs ( when mint upload image , or second step ? I think second step)
+    //2. Shipment status returns string
+    //3. think about events
+    //4. read more state to string to read in the front end
+    //5. bought item state for buyer, he will be able to view the items that he bought and shippment status in his page , custom route with address
+    //6. shipment address can be viewed only by seller that sold item to the buyer
