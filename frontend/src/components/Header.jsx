c94f8feaf2ba82fb9574959f89fded233d351409
AriaWorkshop <ariaworkshopnft@gmail.com>
2023-02-12 15:11:28 +0200

    disable selecting on header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='grid grid-cols-1 items-center'>
+    <div className='grid grid-cols-1 items-center' style={{
+        userSelect: 'none',
+    }}>
         
         <p className='flex justify-center text-6xl mt-20'>Buy and sell goods secured by  <span className='ml-3 text-blue-600'>smart contracts</span></p>
     </div>
