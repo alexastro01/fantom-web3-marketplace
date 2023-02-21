4bc394be3fa7ce10c9a94cf80a718539f7d54b3e
AriaWorkshop <ariaworkshopnft@gmail.com>
2023-02-21 15:12:19 +0200

    header add

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
         userSelect: 'none',
     }}>
         
-        <p className='flex justify-center text-6xl mt-20'>Buy and sell goods secured by  <span className='ml-3 text-blue-600'>smart contracts</span></p>
+        <p className='flex justify-center text-6xl mt-20'>Buy and sell physical goods secured by  <span className='ml-3 text-blue-600'>smart contracts</span></p>
         <p className='flex justify-center text-5xl my-12'>NFTs as <span className='ml-3 text-blue-600'>proof of purchase</span></p>
     
     </div>
